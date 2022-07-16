8127a63d8964fe5e0186d7f3811112b27bd2b76d
Rafael Moreira <rafaeltimoreirass@gmail.com>
2022-07-16 02:22:16 -0300

    Link

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/rafaelmoreiraa"> @rafaelmoreiraa</a>
+                    <a href="https://www.instagram.com/rafaelmoreiraa" target="_blank"> @rafaelmoreiraa</a>
                 </p>
             </div>
         </header>
